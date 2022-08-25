433b0db4f9478f722f3728076671d1a218e1d13c
maiaflow <maiacanter@gmail.com>
2022-08-25 16:51:10 -0700

    ima javacript

--- 4.base-apparel-coming-soon-master/script.js
+++ 4.base-apparel-coming-soon-master/script.js
@@ -1,10 +1,2 @@
-const email = document.getElementById("mail");
-
-email.addEventListener("input", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an e-mail address!");
-    email.reportValidity();
-  } else {
-    email.setCustomValidity("");
-  }
-});
+const message = "Please provide a valid email";
+document.querySelector('span.error').innerHTML = message;
