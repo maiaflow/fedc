a136f124e2af0c1db4b6aeb1618822cc3c094b57
maiaflow <maiacanter@gmail.com>
2022-08-30 13:41:04 -0700

    did i just do it

--- 4.base-apparel-coming-soon-master/script.js
+++ 4.base-apparel-coming-soon-master/script.js
@@ -1,2 +1,11 @@
+const inputs = document.querySelectorAll('input');
 const message = "Please provide a valid email";
-document.querySelector('span.error').innerHTML = message;
+const nomessage = "no";
+
+for (const input of inputs) {
+  if (input.matches(':invalid')) {
+    document.querySelector('span.error').innerHTML = message;
+  } else {
+    document.querySelector('span.error').innerHTML = nomessage;
+  }
+}
