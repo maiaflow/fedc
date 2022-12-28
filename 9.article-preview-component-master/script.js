82a8a9cbbaa99b5fcac591bb3423d43715d49379
maiaflow <maiacanter@gmail.com>
2022-12-28 09:26:02 -0800

    else unset

--- 9.article-preview-component-master/script.js
+++ 9.article-preview-component-master/script.js
@@ -8,5 +8,7 @@ const targetDiv = document.getElementById("pop");
         shArrow.style.filter = "grayscale(1) brightness(3)";
       } else {
         targetDiv.style.display = "none";
+        btn.style.background = "hsl(210, 46%, 95%)";
+        shArrow.style.filter = "unset";
       }
     };
