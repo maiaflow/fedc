6659a0004f1a3a4500959bf0eb8bdedb373fbd32
maiaflow <maiacanter@gmail.com>
2022-12-28 09:24:03 -0800

    js slay

---
+++ 9.article-preview-component-master/script.js
@@ -0,0 +1,12 @@
+const targetDiv = document.getElementById("pop");
+    const btn = document.getElementById("icon");
+    const shArrow = document.getElementById("sharrow");
+    btn.onclick = function () {
+      if (targetDiv.style.display !== "block") {
+        targetDiv.style.display = "block";
+        btn.style.background = "hsl(214, 17%, 51%)";
+        shArrow.style.filter = "grayscale(1) brightness(3)";
+      } else {
+        targetDiv.style.display = "none";
+      }
+    };
